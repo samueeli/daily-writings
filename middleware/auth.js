4a76e656d110545fc29b4a268401919430016cc4
samueeli <samuli.salminen@raivo.io>
2022-10-31 12:35:23 +0200

    added authetication miidleware and auth routes

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  //get token from header
+  const token = req.header('x-auth-token');
+
+  // check if token is not found
+  if (!token) {
+    return res.status(401).json({ msg: 'Authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'No valid token' });
+  }
+};
